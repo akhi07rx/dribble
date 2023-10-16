65ea9fb032501fa30cea0648f7ec1fb5cb5e35e1
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-16 19:35:07 +0530

    0.21

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,38 @@ class WebViewEx extends StatefulWidget {
 }
 
 class _WebViewExState extends State<WebViewEx> {
-  final WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse("https://dribbble.com/"));
+  late Future<WebViewController> _controllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerFuture = _initWebViewController();
+  }
+
+  Future<WebViewController> _initWebViewController() async {
+    WebViewController controller = WebViewController();
+    await controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    await controller.loadRequest(Uri.parse('https://dribbble.com/'));
+    return controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(controller: controller),
+        child: FutureBuilder<WebViewController>(
+          future: _controllerFuture,
+          builder: (BuildContext context,
+              AsyncSnapshot<WebViewController> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return WebViewWidget(controller: snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error loading web view'));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
